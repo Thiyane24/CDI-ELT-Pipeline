288d195bcf09057a18934d44c3d5366a561062ca
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-05-09 03:58:27 +0800

    added the getVideoresults to the video_stats.py

--- video_stats.py
+++ video_stats.py
@@ -28,14 +28,52 @@ def getPlaylistId():
             }
             new_data.append(flat_dict)
         df = pd.DataFrame(new_data)
-        
+        print(df['playlist_id'])
+        return df['playlist_id'].iloc[0]
    else:
        print(response.status_code)
        raise Exception("Status code is not 200")
 
-   print(df['playlist_id'])
+   
   except requests.exceptions.RequestException as e:
        raise e
-  
+   
+
+max_results=50
+
+
+
+#gets the video IDs
+def getVideoResult(playlist_id):
+    base_url= f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlist_id}&key={api_key}"
+    
+    
+    
+    try: 
+        response1 = requests.get(base_url)
+        data1= response1.json()
+        print(json.dumps(data1, indent=4))
+        print("\n")
+        video_ids = []
+        if response1.status_code==200:
+            for items in data1['items']:
+                video_ids.append(items['contentDetails']['videoId'])
+                
+            df1 = pd.DataFrame(video_ids, columns=['videoId'])
+            print(df1)
+            return df1
+        
+        else:
+            print(response1.status_code)
+            
+        
+    except requests.exceptions.RequestException as e:
+        raise e
+    except KeyError as e:
+        print(f"Unexpected response structure: {e}")        
+    
+    
+    
 if __name__ == "__main__":
-    getPlaylistId()
+    playlist_id= getPlaylistId()
+    getVideoResult(playlist_id)
