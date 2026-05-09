59d635a9416ff061df6fbe2684c1bfac45f9d37d
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-05-09 18:39:08 +0800

    added the extraction of the video data

--- video_stats.py
+++ video_stats.py
@@ -73,7 +73,50 @@ def getVideoResult(playlist_id):
         print(f"Unexpected response structure: {e}")        
     
     
+
+def video_data(video_ids):
+    extracted_data = []
+
+    def batch_lst(video_id_lst, batch_size):
+        for i in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[i: i + batch_size]
+
+    try:
+        for batch in batch_lst(video_ids, max_results):
+            video_ids_str = ",".join(batch)
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id={video_ids_str}&key={api_key}"  
+            response = requests.get(url)
+            data = response.json()
+            print(json.dumps(data, indent=4)) 
+            print(f"Batch size: {len(batch)}")
+            print(f"Items returned: {len(data.get('items', []))}")
+
+            for item in data.get('items', []):  
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_dict = {                          
+                    'video_id': item['id'],             
+                    'title': snippet['title'],
+                    'publishedAt': snippet['publishedAt'],
+                    'duration': contentDetails['duration'],
+                    'viewCount': statistics.get('viewCount', None),    
+                    'likeCount': statistics.get('likeCount', None),
+                    'commentCount': statistics.get('commentCount', None)
+                }
+                extracted_data.append(video_dict)    
+        
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    df2 = pd.DataFrame(extracted_data)
+    print(df2)
+    return df2   
     
+
+ 
 if __name__ == "__main__":
     playlist_id= getPlaylistId()
-    getVideoResult(playlist_id)
+    video_ids = getVideoResult(playlist_id)
+    video_data(video_ids['videoId'].tolist()) 
